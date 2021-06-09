eb7187444e1bb696dd5175be6131a3d4b7525f36
Edgar Acosta <edazam05@gmail.com>
2021-06-08 22:56:38 -0500

    agregado_template

--- cuyes/urls.py
+++ cuyes/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='cuy-overview'),
-    path('cuy-list/', views.cuyList, name='cuy-list')
+    path('cuy-list/', views.cuyList, name='cuy-list'),
+    path('home', views.home, name="Home"),
+    path('nosotros/', views.nosotros, name="Nosotros"),
+    path('tienda/', views.tienda, name="Tienda"),
+    path('contacto/', views.contacto, name="Contacto"),
+
 ]
--- cuyproject/urls.py
+++ cuyproject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('cuy/', include('cuyes.urls')),
+    path('', include('cuyes.urls')),
     path('admin/', admin.site.urls),
 ]
